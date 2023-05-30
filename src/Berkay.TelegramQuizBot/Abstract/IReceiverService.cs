e5a51c995a8da2860cb31c9fa71e8514622523e7
biproberkay <biproberkay@gmail.com>
2023-05-30 15:37:02 +0300

    first working demo.

---
+++ src/Berkay.TelegramQuizBot/Abstract/IReceiverService.cs
@@ -0,0 +1,9 @@
+namespace Berkay.TelegramQuizBot.Abstract;
+
+/// <summary>
+/// A marker interface for Update Receiver service
+/// </summary>
+public interface IReceiverService
+{
+    Task ReceiveAsync(CancellationToken stoppingToken);
+}
---
+++ src/Berkay.TelegramQuizBot/Services/ReceiverService.cs
@@ -0,0 +1,16 @@
+using Telegram.Bot;
+using Berkay.TelegramQuizBot.Abstract;
+
+namespace Berkay.TelegramQuizBot.Services;
+
+// Compose Receiver and UpdateHandler implementation
+public class ReceiverService : ReceiverServiceBase<UpdateHandler>
+{
+    public ReceiverService(
+        ITelegramBotClient botClient,
+        UpdateHandler updateHandler,
+        ILogger<ReceiverServiceBase<UpdateHandler>> logger)
+        : base(botClient, updateHandler, logger)
+    {
+    }
+}
